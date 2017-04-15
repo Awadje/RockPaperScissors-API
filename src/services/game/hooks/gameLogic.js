a9fa2b5c96dc7a99a9b8b774251c973610e7ecb7
Awadje <michaelawad85@gmail.com>
2017-04-15 14:15:32 +0200

    Gamelogic operational

--- src/services/game/hooks/gameLogic.js
+++ src/services/game/hooks/gameLogic.js
@@ -5,43 +5,33 @@ module.exports = function(options) {
    return hook.app.service('games').get(hook.id)
    .then((game) =>{
 
-          console.log("starting gamelogic");
-
-    //  if  (hook.data.PlayerInput[0].length === 2){
-
-       if (hook.data.PlayerInput[0].indexOf(1) === 1 && hook.data.PlayerInput[1].indexOf(2) === 2) {
-          return hook.data.winner = game.playerIds[0]
-          console.log("firststatement");
+       if (game.PlayerInputOne === 1 && game.PlayerInputTwo === 2)  {
+          hook.data.winner = game.playerIds[0]
         }
 
-
-         else if (hook.params.user.PlayerInputOne ===2 && hook.params.user.PlayerInputTwo ===1) {
-            return hook.data.winner = game.playerIds[0]
+        else if (game.PlayerInputOne === 2 && game.PlayerInputTwo === 1)  {
+           hook.data.winner = game.playerIds[0]
          }
 
-         else if (hook.params.user.PlayerInputOne ===3 && hook.params.user.PlayerInputTwo ===2) {
-              return  hook.data.winner = game.playerIds[0]
+         else if (game.PlayerInputOne === 3 && game.PlayerInputTwo === 2)  {
+            hook.data.winner = game.playerIds[0]
+          }
+
+          else if (game.PlayerInputOne === 2 && game.PlayerInputTwo === 1)  {
+             hook.data.winner = game.playerIds[1]
+           }
+
+           else if (game.PlayerInputOne === 1 && game.PlayerInputTwo === 2)  {
+              hook.data.winner = game.playerIds[1]
+            }
+
+           else if (game.PlayerInputOne === 1 && game.PlayerInputTwo === 2)  {
+                hook.data.winner = game.playerIds[1]
+                 }
+
+            else if (game.PlayerInputOne === game.PlayerInputTwo) {
+              return;
              }
-
-         else if (hook.params.user.PlayerInputOne ===2 && hook.params.user.PlayerInputTwo ===1) {
-               return hook.data.winner = game.playerIds[1]
-             }
-
-         else if (hook.params.user.PlayerInputOne ===1 && hook.params.user.PlayerInputTwo ===2) {
-               return hook.data.winner = game.playerIds[1]
-             }
-
-         else if (hook.params.user.PlayerInputOne ===1 && hook.params.user.PlayerInputTwo ===3) {
-               return hook.data.winner = game.playerIds[1]
-             }
-         else {
-
-         }
-
-      //  }
-
-
-
-    })
-  }
-}
+       })
+     }
+   }
